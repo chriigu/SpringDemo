2366cda2f14601f8b3bb6f7ca5f050c8fc7397c6
Chrigu <christian.bigler@hotmail.com>
2025-04-18 22:05:37 +0200

    Implemented dark mode

--- AngularDemo/src/app/app.component.ts
+++ AngularDemo/src/app/app.component.ts
@@ -3,10 +3,11 @@ import {NavigationEnd, Router} from '@angular/router';
 import {filter} from 'rxjs';
 import {HomeComponent} from './core/components/home/home.component';
 import {SideNavBarComponent} from './core/components/sidenav/side-nav-bar/side-nav-bar.component';
+import {HeaderComponent} from './core/components/header/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, SideNavBarComponent],
+  imports: [HomeComponent, SideNavBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
---
+++ AngularDemo/src/app/core/components/header/header/header.component.ts
@@ -0,0 +1,30 @@
+import {Component, Renderer2} from '@angular/core';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatSlideToggle
+  ],
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  darkModeEnabled = false;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  toggleDarkMode(event: any)
+  {
+    this.darkModeEnabled = event.checked;
+    console.log('Dark mode enabled:', this.darkModeEnabled);
+    if(this.darkModeEnabled)
+    {
+      this.renderer.addClass(document.body, 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark');
+    }
+  }
+}
